3a2cf9ff89f55860e34aacf02d616bed9a1baae1
Halldor <njashi9@gmail.com>
2023-03-23 18:28:45 +0000

    Eitthvað að fikta við að setja upp players og snúa borðinu rétt

--- snakes.cpp
+++ snakes.cpp
@@ -1,12 +1,15 @@
 #include <SFML/Graphics.hpp>
 #include <chrono>
+#include <iostream>
 
 using namespace std;
 
 int main()
 {
-    sf::RenderWindow window(sf::VideoMode(500, 500), "Snakes and Ladders");
+    sf::RenderWindow window(sf::VideoMode(700, 500), "Snakes and Ladders");                 // x = 700 til að fá smá pláss til hliðar (500 default)
 	srand(std::chrono::system_clock::to_time_t(std::chrono::system_clock::now()));
+    
+    // PLAYFIELD CONSTRUCTION
     const int numSquares = 100;
     const int squareSize = 50;
     sf::RectangleShape squares[numSquares];
@@ -22,14 +25,30 @@ int main()
         }
     }
 
+    // NUMBERS INSERTED TO PLAYFIELD
     sf::Font font;
     if (!font.loadFromFile("arial.ttf")) {
         return EXIT_FAILURE;
     }
     sf::Text numbers[numSquares];
+    
+    int board_numb = 100;          //bætti við
+    bool board_number_switch = true;
+
     for (int i = 0; i < numSquares; i++) {
         numbers[i].setFont(font);
-        numbers[i].setString(std::to_string(i+1));
+        if(board_number_switch){
+            numbers[i].setString(std::to_string(board_numb - i)); 
+        }
+        else{
+            numbers[i].setString(std::to_string(board_numb - 10 + (i % 10)));
+        }
+
+        if(i % 10 == 0){
+            board_number_switch = !board_number_switch;
+        }
+
+        //numbers[i].setString(std::to_string(board_numb - i));           // í staðinn fyrir i + 1
         numbers[i].setFillColor(sf::Color::Black);
         numbers[i].setCharacterSize(20);
         
@@ -41,6 +60,8 @@ int main()
                                 squares[i].getPosition().y + squareSize / 2.f);
     }
 
+
+    // SNAKES AND LADDERS
     sf::VertexArray lines(sf::Lines, 4);
     lines[0].position = sf::Vector2f(squares[2].getPosition().x + squareSize / 2.f,
                                      squares[2].getPosition().y + squareSize / 2.f);
@@ -56,29 +77,73 @@ int main()
     lines[3].color = sf::Color::Green;
 
 
-    sf::CircleShape playerDot(squareSize / 4.f);
+    // INFORMATION WINDOW ON THE SIDE
+
+    // PLAYER ICON POSITION VARIABLES
+    float player1posx = 1.6;
+    float player1posy = 1.5;
+    float player2posx = 3.2;      // þarf að skoða
+    float player2posy = 1.5; 
+
+    // PLAYER ICON SETUP
+    sf::CircleShape playerDot(squareSize / 8.f);
     playerDot.setFillColor(sf::Color::Black);
-    playerDot.setPosition(squares[0].getPosition().x + squareSize / 2.f,
-                           squares[0].getPosition().y + squareSize / 2.f);
+    // playerDot.setPosition(squares[0].getPosition().x + squareSize / player1posx,                   //1.6f      upprunalega 2.f
+    //                        squares[0].getPosition().y + squareSize /player1posy);                 //1.5f      upprunalega 2.f
+    playerDot.setPosition(squares[99].getPosition().x + squareSize / player1posx,                   //0 í 99
+                           squares[99].getPosition().y + squareSize /player1posy);                 // 0 í 99
 
-    int playerPosition = 0;
+    // PLAYER2 ICON SETUP
+    sf::CircleShape playerDot2(squareSize / 8.f);
+    playerDot2.setFillColor(sf::Color::Yellow);
+    playerDot2.setPosition(squares[99].getPosition().x + squareSize / player2posx,                   
+                           squares[99].getPosition().y + squareSize /player2posy);                 
+    
 
+    // PLAYER POSTIION INITIAL
+    int playerPosition = 99;
+    int player2Position = 99;
+
+    bool Player1Turn = true;
     while (window.isOpen())
     {
-        sf::Event event;
+
+
+        sf::Event event;     
         while (window.pollEvent(event))
         {
             if (event.type == sf::Event::Closed)
                 window.close();
 
-            // Move the player
-            if (event.type == sf::Event::KeyPressed && event.key.code == sf::Keyboard::R) {
+            // Move the player (player1)
+            if (event.type == sf::Event::KeyPressed && event.key.code == sf::Keyboard::R && Player1Turn) {
                 int diceRoll = std::rand() % 6 + 1; 
-                playerPosition += diceRoll;        
-                sf::Vector2f newPosition = squares[playerPosition].getPosition() + sf::Vector2f(squareSize / 2.f, squareSize / 2.f);
+                cout << "Player 1 rolled: " << diceRoll << endl;
+                playerPosition -= diceRoll;        
+                sf::Vector2f newPosition = squares[playerPosition].getPosition() + sf::Vector2f(squareSize /  player1posx, squareSize / player1posy);        //upprunalega 2.f á báðum stöðum
                 playerDot.setPosition(newPosition);
+                
+                if (playerPosition <= 0){           // ein leið til að stoppa leikinn
+                    window.close();
+                }
+                Player1Turn = false;
             }
+            // Move the player (player2)
+            else if(event.type == sf::Event::KeyPressed && event.key.code == sf::Keyboard::R && !Player1Turn) {
+                int diceRoll = std::rand() % 6 + 1; 
+                cout << "Player 2 rolled: " << diceRoll << endl;
+                player2Position -= diceRoll;        
+                sf::Vector2f newPosition = squares[player2Position].getPosition() + sf::Vector2f(squareSize /  player2posx, squareSize / player2posy);        
+                playerDot2.setPosition(newPosition);
+                
+                if (player2Position <= 0){           // ein leið til að stoppa leikinn
+                    window.close();
+                }
+                Player1Turn = true;
+            }
+            
         }
+
 
         window.clear();
         
@@ -88,6 +153,7 @@ int main()
         }
         window.draw(lines);
         window.draw(playerDot);
+        window.draw(playerDot2);
         window.display();
     }
 
